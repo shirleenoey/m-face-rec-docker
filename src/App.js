063488bf0f4a29c1db5039d4162b958c6090f297
shirleen <shirleenoey@gmail.com>
2023-05-05 10:29:56 +0700

    finishing user auth

--- src/App.js
+++ src/App.js
@@ -49,7 +49,20 @@ class App extends Component {
         .then(resp => resp.json())
         .then(data => {
           if (data && data.id) {
-            console.log('success we need to get user data')
+            fetch(`http://localhost:3000/profile/${data.id}`, {
+              method: 'get',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+              }
+            })
+              .then(resp => resp.json())
+              .then(user => {
+                if (user && user.email) {
+                  this.loadUser(user);
+                  this.onRouteChange('home');
+                }
+              })
           }
         })
         .catch(console.log)
